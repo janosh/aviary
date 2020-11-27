1871d27e4d63d124573fcc8b59ae1b2ff273d481
Janosh Riebesell <janosh.riebesell@gmail.com>
2020-11-27 19:20:45 +0100

    wren_volume_error_correlation add spacegroup analysis

--- notebooks/wren_volume_error_correlation.py
+++ notebooks/wren_volume_error_correlation.py
@@ -3,6 +3,8 @@ import matplotlib as mpl
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+import seaborn as sns
+from scipy.stats import zscore
 from sklearn.metrics import r2_score
 
 from roost.base import ROOT
@@ -21,9 +23,48 @@ plt.rcParams["font.size"] = 14
 log_color_scale = mpl.colors.LogNorm()
 
 
-# %% data paths
+# %%
 df_path = lambda idx: f"{ROOT}/models/mp-subset/ens_{idx}/test_results.csv"
 
+# disable na_filter so the composition NaN is not converted to nan
+dfs = [pd.read_csv(df_path(idx), na_filter=False) for idx in range(20)]
+total = pd.concat(dfs).set_index("id")
+
+# sanity check
+# verify each material id appeared about (ensemble size x test set size) times
+# in the test set, e.g. 20 x 0.2 = 4 (actual result: 4.049)
+total.index.value_counts().mean()
+total["res"] = total.target - total.pred_0
+total["ae"] = (total.target - total.pred_0).abs()
+
+
+# %%
+df_mp = pd.read_csv(ROOT + "/data/datasets/large/mat_proj.csv", na_filter=False)
+df_mp = df_mp.rename(columns={"material_id": "id"}).set_index("id")
+
+
+# %%
+for key in [
+    "composition",
+    "formation_energy_per_atom",
+    "e_above_hull",
+    "band_gap",
+    "nelements",
+    "nsites",
+    "volume",
+    "spacegroup",
+]:
+    total[key] = df_mp[key]
+
+
+# %%
+# total.to_csv(f"{ROOT}/models/mp-subset/combined_test_results.csv", float_format="%g")
+total = pd.read_csv(
+    f"{ROOT}/models/mp-subset/combined_test_results.csv", index_col="id"
+)
+
+
+# %% data paths
 dfs = [pd.read_csv(df_path(idx), comment="#", na_filter=False) for idx in range(3)]
 
 
@@ -47,35 +88,6 @@ for df, cell in zip(dfs, outer_grid):
     )
 
 plt.savefig(f"{ROOT}/models/mp-subset/density-scatter.png")
-
-
-# %%
-# disable na_filter so the composition NaN is not converted to nan
-dfs = [pd.read_csv(df_path(idx), na_filter=False) for idx in range(20)]
-total = pd.concat(dfs).set_index("id")
-# sanity check
-# verify each material id appeared about (ensemble size x test set size) times
-# in the test set, e.g. 20 x 0.2 = 4 (actual result: 4.049)
-total.index.value_counts().mean()
-total["ae"] = (total.target - total.pred_0).abs()
-
-
-# %%
-df_mp = pd.read_csv(ROOT + "/data/datasets/large/mat_proj.csv", na_filter=False)
-df_mp = df_mp.rename(columns={"material_id": "id"}).set_index("id")
-# add crystal unit cell volume to results dataframe
-total["volume"] = df_mp.volume
-total["composition"] = df_mp.composition
-
-
-# %%
-for key in ["volume", "composition", "e_above_hull", "band_gap", "nsites"]:
-    total[key] = df_mp[key]
-
-
-# %%
-# total.to_csv(f"{ROOT}/models/mp-subset/combined_test_results.csv", float_format="%g")
-total = pd.read_csv(f"{ROOT}/models/mp-subset/combined_test_results.csv")
 
 
 # %%
@@ -177,3 +189,35 @@ plt.savefig(f"{ROOT}/models/mp-subset/hex-pred-vs-target-color-by-volume-log-mea
 cum_err_cum_res(targets, preds, [title])
 
 plt.savefig(f"{ROOT}/models/mp-subset/cum_err_cum_res.png")
+
+
+# %%
+top_spacegroups = total.value_counts("spacegroup").head(20).index
+no_outliers = total[np.abs(zscore(total.res)) < 0.5]
+
+
+# %%
+sns.violinplot(
+    data=no_outliers.loc[lambda x: x.spacegroup.isin(top_spacegroups)],
+    x="spacegroup",
+    y="res",
+    scale="count",
+    cut=0,
+)
+plt.savefig(f"{ROOT}/models/mp-subset/violin-ae-vs-spacegroup.png")
+
+
+# %%
+# remove outliers (https://stackoverflow.com/a/23202269)
+total[np.abs(zscore(total.res)) < 0.1]
+
+
+# %%
+# the spacegroups with highest MAE and their number of samples
+top_mae_spacegroups = pd.DataFrame(
+    total.groupby("spacegroup").ae.mean().sort_values(ascending=False).head(10)
+).rename(columns={"ae": "mae"})
+top_mae_spacegroups["count"] = pd.DataFrame(total.value_counts("spacegroup"))
+
+top_mae_spacegroups.plot(kind="bar", y=["mae", "count"], secondary_y="count")
+plt.savefig(f"{ROOT}/models/mp-subset/bar-mae+count-vs-spacegroup.pdf")
